edc77f120e096bcbedc886ed69de4d70f8bef776
SatriaMulyaa <satriaamulyaa@gmail.com>
2022-03-23 22:02:23 +0700

    type check,cast,instanceof

--- src/PolymorphismApp.java
+++ src/PolymorphismApp.java
@@ -2,7 +2,7 @@ public class PolymorphismApp {
     public static void main(String[] args) {
         Employee employee = new Employee("Satria");
         employee.sayHello("Budi");
-
+//        Manager manager = new Manager("rudi");
         employee = new Manager("Satria", "jakarta");
         employee.sayHello("Budi");
 
@@ -13,9 +13,21 @@ public class PolymorphismApp {
         sayHello(new Manager("jiko"));
         sayHello(new VicePresident("jike"));
 
+//        System.out.println(manager instanceof Employee);
+//        System.out.println(employee instanceof Manager);
+
     }
 
-    static void sayHello(Employee employee){
-        System.out.println("hallo " + employee.name) ;
-    }
+    static void sayHello(Employee employee) {
+        if (employee instanceof VicePresident) {
+            VicePresident vicePresident = (VicePresident) employee;
+            System.out.println("hallo  " + vicePresident.name) ;
+        }else if(employee instanceof Manager){
+            Manager manager = (Manager) employee;
+            System.out.println("hallo Manager " + manager.name) ;
+        }else{
+            System.out.println("hallo Employee " + employee.name) ;
+        }
+     }
 }
+
