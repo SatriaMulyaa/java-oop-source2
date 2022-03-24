fb2d79d848adc0c01ceff9a0c256e1e7c89d62d1
SatriaMulyaa <satriaamulyaa@gmail.com>
2022-03-24 20:24:24 +0700

    import

--- src/programmer/zaman/now/application/Application.java
+++ src/programmer/zaman/now/application/Application.java
@@ -1,11 +1,16 @@
 package programmer.zaman.now.application;
 
-import programmer.zaman.now.data.Product;
+import programmer.zaman.now.data.*;
+
+
 
 public class Application {
     public static void main(String[] args) {
         Product product = new Product("macbook", 3000000);
         System.out.println(product.name);
         System.out.println(product.price);
+//        String eko = "eko";
+//        Integer one = 1;
+        Data data = new Data();
     }
 }
