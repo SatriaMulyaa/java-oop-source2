0dff73b3f5a04454bcebe83e9cca62868729e751
SatriaMulyaa <satriaamulyaa@gmail.com>
2022-03-25 10:53:06 +0700

    abstract method

---
+++ src/programmer/zaman/now/application/AnimalApp.java
@@ -0,0 +1,11 @@
+package programmer.zaman.now.application;
+
+import programmer.zaman.now.data.*;
+
+public class AnimalApp {
+    public static void main(String[] args) {
+        Animal animal = new Cat();
+        animal.name = "Satria";
+        animal.run();
+    }
+}
---
+++ src/programmer/zaman/now/data/Cat.java
@@ -0,0 +1,8 @@
+package programmer.zaman.now.data;
+
+public class Cat extends Animal{
+
+    public void run(){
+        System.out.println("cat " + name + " is run");
+    }
+}
